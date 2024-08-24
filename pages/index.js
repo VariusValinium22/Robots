ac75e46b90e79c71e346b5e6c3a19de5ecef4302
Martin Young <MartingYoung@hotmail.com>
2024-08-24 14:02:03 -0500

    1st commit description

---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import Section from "../components/Section.js";
+import DefaultCard from "../components/DefaultCard.js";
+import HorizontalCard from "../components/HorizontalCard.js";
+import FilterButton from "../components/FilterButton.js";
+import {
+  defaultCardButton,
+  horizontalCardButton,
+  cardListSelector,
+  items,
+  filterButtons,
+  filterListSelector,
+  filterButtonTemplate
+} from "../utils/constants.js";
+
+const defaultCardList = new Section({
+  data: items,
+  renderer: (item) => {
+    const card = new DefaultCard(item, ".default-card");
+    const cardElement = card.generateCard();
+    defaultCardList.setItem(cardElement);
+  }
+}, cardListSelector);
+
+const horizontalCardList = new Section({
+  data: items,
+  renderer: (item) => {
+    const card = new HorizontalCard(item, ".horizontal-card");
+    const cardElement = card.generateCard();
+    horizontalCardList.setItem(cardElement);
+  }
+}, cardListSelector);
+
+//render the toggle switches
+const filterList = new Section({
+  data: filterButtons,
+  renderer: (item) => {
+    const filterButton = new FilterButton({data: item}, FilterButtonTemplate);
+    const filterButtonElement = filterButton.generateButton();
+    filterList.setItem(filterButtonElement);
+  }
+  }, filterListSelector);
+
+defaultCardButton.addEventListener("click", () => {
+  defaultCardList.renderItems();
+});
+
+horizontalCardButton.addEventListener("click", () => {
+  horizontalCardList.renderItems();
+});
+
+defaultCardList.renderItems();
+
+filterList.renderItems();
